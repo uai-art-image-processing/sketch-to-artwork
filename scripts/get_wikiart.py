7c152c105d7fe1d981fae48401b1e36c37ebe918
mrMustacho <almardones@alumnos.uai.cl>
2021-11-11 20:28:14 +0000

    bugfixes

--- scripts/get_wikiart.py
+++ scripts/get_wikiart.py
@@ -9,30 +9,15 @@ import pandas as pd
 import numpy as np
 from PIL import Image
 from sklearn.model_selection import train_test_split
-from check_integrity import verifyImg
-
-train_ratio = 0.85
-test_ratio = 0.10
-validation_ratio = 0.05
-            
-def copyimgs(df, dst_path):
-    df.apply(lambda row: shutil.copy(row.path, dst_path) if os.path.isfile(row.path) else None, axis=1)
-
-def clean(file):
-    classes = pd.read_csv(f"{FILE}_class.txt", delimiter=" ", names=[f"{FILE}"])
-    df = pd.read_csv(file, delimiter=",", names=["path", f"{FILE}"])
-    df[f"{FILE}"] = df[f"{FILE}"].apply(lambda x: classes[f"{FILE}"][x])
-    # df["path"] = df["path"].apply(lambda x: SRC_PATH+x)
-    return df
 
 def mergeCSV(data_dir):
-    artist_train = pd.read_csv(os.path.join(data_dir,"artist_train.csv"), delimiter=",", names=["path", "artist"])
-    genre_train = pd.read_csv(os.path.join(data_dir,"genre_train.csv"), delimiter=",", names=["path", "genre"])
-    style_train = pd.read_csv(os.path.join(data_dir,"style_train.csv"), delimiter=",", names=["path", "style"])
+    artist_train = pd.read_csv(os.path.join(data_dir,"artist_train.csv"), encoding='utf8', delimiter=",", names=["path", "artist"])
+    genre_train = pd.read_csv(os.path.join(data_dir,"genre_train.csv"), encoding='utf8', delimiter=",", names=["path", "genre"])
+    style_train = pd.read_csv(os.path.join(data_dir,"style_train.csv"), encoding='utf8', delimiter=",", names=["path", "style"])
 
-    artist_val = pd.read_csv(os.path.join(data_dir,"artist_val.csv"), delimiter=",", names=["path", "artist"])
-    genre_val = pd.read_csv(os.path.join(data_dir,"genre_val.csv"), delimiter=",", names=["path", "genre"])
-    style_val = pd.read_csv(os.path.join(data_dir,"style_val.csv"), delimiter=",", names=["path", "style"])
+    artist_val = pd.read_csv(os.path.join(data_dir,"artist_val.csv"), encoding='utf8', delimiter=",", names=["path", "artist"])
+    genre_val = pd.read_csv(os.path.join(data_dir,"genre_val.csv"), encoding='utf8', delimiter=",", names=["path", "genre"])
+    style_val = pd.read_csv(os.path.join(data_dir,"style_val.csv"), encoding='utf8', delimiter=",", names=["path", "style"])
 
     artist = pd.concat([artist_train, artist_val])
     genre = pd.concat([genre_train, genre_val])
@@ -40,7 +25,7 @@ def mergeCSV(data_dir):
 
     data = pd.merge(genre, style, on="path", how="outer")
     data = pd.merge(data, artist, on="path", how="outer")
-    data["path"] = data["path"].apply(lambda path: os.path.join(os.getcwd(), data_dir, "wikiart",path))
+    data["path"] = data["path"].apply(lambda path: os.path.join(os.getcwd(), data_dir, path))
 
     return data
 
@@ -67,13 +52,13 @@ def main(args):
     val = None
     if args.val != None:
         val, test = train_test_split(test, test_size=args.test/(args.test + args.val), shuffle=False)
-        print("Spit size:".lent(train),len(test),len(val))
+        print("Spit size:",len(train),len(test),len(val))
     else:
-        print("Spit size:".len(train),len(test))
+        print("Spit size:",len(train),len(test))
 
-    np.savetxt(os.path.join(os.getcwd(),"datasets/wikiart_train.txt"), train)
-    np.savetxt(os.path.join(os.getcwd(),"datasets/wikiart_test.txt"), test)
-    if val is not None: np.savetxt(os.path.join(os.getcwd(),"datasets/wikiart_val.txt"), val)
+    np.savetxt(os.path.join(os.getcwd(),"datasets/wikiart_train.txt"), train, fmt="%s")
+    np.savetxt(os.path.join(os.getcwd(),"datasets/wikiart_test.txt"), test, fmt="%s")
+    if val is not None: np.savetxt(os.path.join(os.getcwd(),"datasets/wikiart_val.txt"), val, fmt="%s")
 
     print("Done splitting dataset")
 
